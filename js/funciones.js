99282e9ff4f7c32bb5b909712c21917910b9343a
FranciscoPuchala <122131800+FranciscoPuchala@users.noreply.github.com>
2026-06-13 18:31:18 -0300

    l

--- js/funciones.js
+++ js/funciones.js
@@ -6,6 +6,8 @@ document.getElementById("agregarArticulo").addEventListener("click", agregarArti
 document.getElementById("agregarventas").addEventListener("click", agregarventa)
 document.getElementById("cancelarventa").addEventListener("click", cancelarventa)
 document.getElementById("botonAgregar").addEventListener("click",agregarDatosInfluencer)
+document.getElementById("botonAgregarArticulo").addEventListener("click",agregarDatosArticulo)
+document.getElementById("botonAgregarVenta").addEventListener("click",agregarDatosVenta)
 
 function agregarInfluencer(){
     document.getElementById("dialogInfluencer").showModal()
@@ -40,6 +42,8 @@ function renderizarTabla(){
         fila.innerHTML = "<td>" + inf.nombre + "</td>" +
                          "<td>" + inf.email + "</td>" +
                          "<td>" + inf.comision + "</td>" +
+                         "<td></td>" +
+                         "<td></td>" +
                          "<td><button class='boton1'>Ventas</td>"
         tbody.appendChild(fila)
     }
@@ -54,4 +58,56 @@ function agregarDatosInfluencer(){
     sistema.influencers.push(nuevoInfluencer)
     renderizarTabla()
 }
+
+function renderizarTablaArticulo(){
+    let tbody = document.getElementById("tbodyArticulos")
+    tbody.innerHTML = "" //borra la info que hay
+    for(let inf of sistema.articulos){
+        let fila = document.createElement("tr")
+        fila.innerHTML = "<td>" + inf.codigo + "</td>" +
+                         "<td>" + inf.descripcion + "</td>" +
+                         "<td>" + inf.precio + "</td>" 
+        tbody.appendChild(fila)
+    }
+
+}
+
+function agregarDatosArticulo(){
+    let codigo = document.getElementById("codigo").value
+    let descripcion = document.getElementById("descripción").value
+    let precio = document.getElementById("precio").value
+
+    let nuevoArticulo = new Articulo(codigo,descripcion,precio)
+    sistema.articulos.push(nuevoArticulo)
+    renderizarTablaArticulo()
+}
+
+function renderizarTablaVenta(){
+    let tbody = document.getElementById("tbodyVentas")
+    tbody.innerHTML = ""
+    for(let ven of sistema.ventas){
+        let fila = document.createElement("tr")
+        fila.innerHTML = "<td>" + ven.numero + "</td>" +
+                         "<td>" + ven.articulo + "</td>" +
+                         "<td>" + ven.influencer + "</td>" +
+                         "<td>" + ven.cantidad + "</td>" +
+                         "<td>" + ven.medio + "</td>" +
+                         "<td><button class='boton1'>❌</button></td>"
+        tbody.appendChild(fila)
+    }
+}
+
+function agregarDatosVenta(){
+    let articulo = document.getElementById("menu").value
+    let influencer = document.getElementById("menu2").value
+    let cantidad = document.getElementById("cantidad").value
+    let medio = document.getElementById("menu3").value
+
+    let nuevaVenta = new Venta(sistema.contadorVentas, articulo, influencer, cantidad, medio)
+    sistema.ventas.push(nuevaVenta)
+    sistema.contadorVentas++
+    renderizarTablaVenta()
+    document.getElementById("dialogventas").close()
+}
+
 
