4b8e082cddddeba8bdf40e77c75f2c20a2f2068d
FranciscoPuchala <122131800+FranciscoPuchala@users.noreply.github.com>
2026-06-13 13:54:20 -0300

    n

--- js/clases.js
+++ js/clases.js
@@ -5,3 +5,30 @@ class Influencer{
         this.comision = comision
     }
 }
+
+class Articulo{
+    constructor(codigo, descripcion, precio){
+        this.codigo = codigo
+        this.descripcion = descripcion
+        this.precio = precio
+    }
+}
+
+class Venta{
+    constructor(numero, articulo, influencer, cantidad, medio){
+        this.numero = numero
+        this.articulo = articulo
+        this.influencer = influencer
+        this.cantidad = cantidad
+        this.medio = medio
+    }
+}
+
+class Sistema{
+    constructor(){
+        this.influencers = []
+        this.articulos = []
+        this.ventas = []
+        this.contadorVentas = 1
+    }
+}
--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,4 @@
+let sistema = new Sistema()
 document.getElementById("agregarInfluencer").addEventListener("click",agregarInfluencer)
 document.getElementById("botonCancelar").addEventListener("click",cancelarInfluencer)
 document.getElementById("botonCancelarArticulo").addEventListener("click",cancelarArticulo)
@@ -31,6 +32,26 @@ function cancelarventa(){
     document.getElementById("dialogventas").close()
 }
 
+function renderizarTabla(){
+    let tbody = document.getElementById("tbodyInfluencers")
+    tbody.innerHTML = "" //borra la info que hay
+    for(let inf of sistema.influencers){
+        let fila = document.createElement("tr")
+        fila.innerHTML = "<td>" + inf.nombre + "</td>" +
+                         "<td>" + inf.email + "</td>" +
+                         "<td>" + inf.comision + "</td>" +
+                         "<td><button class='boton1'>Ventas</td>"
+        tbody.appendChild(fila)
+    }
+
+}
 function agregarDatosInfluencer(){
     let nombre = document.getElementById("nombre").value
+    let email = document.getElementById("email").value
+    let comision = document.getElementById("comision").value
+    
+    let nuevoInfluencer = new Influencer(nombre,email,comision)
+    sistema.influencers.push(nuevoInfluencer)
+    renderizarTabla()
 }
+
